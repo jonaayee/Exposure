7c2b1c869e8a16f512a0e8f7f4c09964823fada0
jonaayee <108772106+jonaayee@users.noreply.github.com>
2024-10-09 15:18:13 -0400

    Initial Commit

---
+++ Exposure/ExposureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExposureApp.swift
+//  Exposure
+//
+//  Created by Jonathan Amburgy on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ExposureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
