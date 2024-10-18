8e4f93b5f0f5311e06fa91287012eab0d548f9fa
jonaayee <108772106+jonaayee@users.noreply.github.com>
2024-10-17 22:13:45 -0400

    kinda worked, didn't have much time

--- Exposure/UI/Core/iOSContentView.swift
+++ Exposure/UI/Core/iOSContentView.swift
@@ -19,62 +19,77 @@ struct iOSContentView: View {
     @State private var pickerItems = [PhotosPickerItem]()
     @State private var selectedImages = [Image]()
     // ----------------------------------------
+    // Errors
+    enum possibleErrors: Error {
+        case someError
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: possibleErrors.someError, code: nil)
+    }
+    
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                ScrollView {
-                    ForEach(0..<selectedImages.count, id: \.self) { i in
-                        selectedImages[i]
-                            .resizable()
-                            .scaledToFit()
+        ZStack {
+            NavigationStack {
+                VStack {
+                    ScrollView {
+                        ForEach(0..<selectedImages.count, id: \.self) { i in
+                            selectedImages[i]
+                                .resizable()
+                                .scaledToFit()
+                        }
                     }
                 }
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Text("Exposure")
-                        .font(.largeTitle)
-                        .bold()
-                }
-                ToolbarItem(placement: .topBarTrailing){
-                    NavigationLink(destination: iOSSettingsView()) {
-                        Image(systemName: "gear.circle")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Text("Exposure")
+                            .font(.largeTitle)
+                            .bold()
                     }
-                }
-                ToolbarItem(placement: .bottomBar) { // need to find a way to seperate them into two different items
-                    HStack {
-                        Picker("Projects view or all photos view.", selection: $viewState) {
-                            ForEach(viewStates, id: \.self) { viewState in
-                                Text(viewState)
+                    ToolbarItem(placement: .topBarTrailing){
+                        NavigationLink(destination: iOSSettingsView()) {
+                            Image(systemName: "gear.circle")
+                        }
+                    }
+                    ToolbarItem(placement: .bottomBar) { // need to find a way to seperate them into two different items
+                        HStack {
+                            Picker("Projects view or all photos view.", selection: $viewState) {
+                                ForEach(viewStates, id: \.self) { viewState in
+                                    Text(viewState)
+                                }
                             }
-                        }
-                        .pickerStyle(.segmented)
-                        .frame(minWidth: 0, maxWidth: 256)
-                        .padding(.trailing, 64)
-                        Button {
-                            print("idk, bring up the share sheet if you feel like it ig...")
-                        } label: {
-                            Image(systemName: "square.and.arrow.up")
-                        }
-                        PhotosPicker(selection: $pickerItems,
-                                     matching: .images
-                        ) {
-                            Image(systemName: "plus")
-                        }
-                        .onChange(of: pickerItems) {
-                            Task {
-                                selectedImages.removeAll()
-                                
-                                for item in pickerItems {
-                                    if let loadedImage = try? await item.loadTransferable(type: Image.self) {
-                                        selectedImages.append(loadedImage)
+                            .pickerStyle(.segmented)
+                            .frame(minWidth: 0, maxWidth: 256)
+                            .padding(.trailing, 64)
+                            Button {
+                                print("idk, bring up the share sheet if you feel like it ig...")
+                            } label: {
+                                Image(systemName: "square.and.arrow.up")
+                            }
+                            PhotosPicker(selection: $pickerItems,
+                                         matching: .images
+                            ) {
+                                Image(systemName: "plus")
+                            }
+                            .onChange(of: pickerItems) {
+                                Task {
+                                    selectedImages.removeAll()
+                                    
+                                    for item in pickerItems {
+                                        if let loadedImage = try? await item.loadTransferable(type: Image.self) {
+                                            selectedImages.append(loadedImage)
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 }
+            }
+            if selectedImages.isEmpty {
+                errorView()
+                    .ignoresSafeArea(.all)
             }
         }
     }
