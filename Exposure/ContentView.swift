6e812f7bcb06dbf95469f3f391084412463bf58c
jonaayee <108772106+jonaayee@users.noreply.github.com>
2024-10-28 23:07:51 -0400

    reset

---
+++ Exposure/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Exposure
+//
+//  Created by Jonathan Amburgy on 10/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
